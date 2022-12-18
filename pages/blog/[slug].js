f897570bdfdc53e23ff3966aaaa76c032c3cfedd
Nenad <68515352+Nenad95M@users.noreply.github.com>
2022-12-18 18:19:33 +0100

    a

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,31 +1,71 @@
 import fs from 'fs';
 import path from 'path';
+import Link from 'next/link';
 import matter from 'gray-matter';
-export default function PostPage(props) {
-  return (
-    <div>
+import { marked } from 'marked';
 
-    </div>
-  )
-}
-export async function getStaticPaths(){
-const files=fs.readdirSync(path.join('posts'));
-const paths=files.map(filename=>{
-    params:{
-        slug:filename.replace('.md', '')
-    }
-})
+import { Layout } from '../../components/Layout';
+import CategoryLabel from '../../components/CategoryLabel';
+export default function PostPage({ frontmatter, content, slug }) {
+    const { title, category, date, cover_image, author, author_image } = frontmatter;
+    console.log(author)
+    return (
+        <Layout title={title}>
+            <div className="w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6">
+                <h1 className="text-5xl mb-7">{title}</h1>
+                <CategoryLabel>
+                    {category}
+                </CategoryLabel>
+                <img src={cover_image} alt={title} className='w-full rounded' />
+                <div className="flex justify-between items-center bg-gray-100 p-2 my-8">
+                    <div className='flex items-center'>
+                        <img src={author_image} alt="" className='mx-4 w-10 h-10 object-cover rounded-full hidden sm:block' />
+                        <h4>{author}</h4>
+                    </div>
+                    <div className="mr-4">
+                        {date}
+                    </div>
+              
+                </div>
+                <div className="blog-text mt-2">
+                         <div dangerouslySetInnerHTML={{__html: marked(content)}}>
 
-return{
-    paths,
-    fallback:false
+                         </div>
+                    </div>
+            </div>
+        </Layout>
+    )
 }
-}
- export async function getStaticProps({params:{slug}}){
-    //hocemo da ucitamo fajl, slug je naziv falja bez ekstencije, pa ga spajamo sa md ekstenzijom
-    const markdownWithMeta=fs.readFileSync(path.join('posts', slug + '.md', 'utf-8'))
-    const {data, content}=matter(markdownWithMeta);
+export async function getStaticPaths() {
+    const files = fs.readdirSync(path.resolve('posts'));
+    const paths = files.map(filename => {
+        return {
+            params: {
+                slug: filename.replace('.md', '')
+            }
+        }
+    })
+
     return {
-        props:{},
+        paths,
+        fallback: false
     }
- }
+}
+export async function getStaticProps({ params: { slug } }) {
+    try {
+        const markdownWithMeta = fs.readFileSync(path.join('posts', slug + '.md'), 'utf-8')
+
+
+        const { data: frontmatter, content } = matter(markdownWithMeta);
+        return {
+            props: {
+                frontmatter,
+                content,
+                slug
+            }
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
---
+++ pages/blog/page/[page_index].js
@@ -0,0 +1,79 @@
+//importujemo node module, mozemo samo da ih koristimo unutar getStaticProps koji ide na serveru
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter'
+import Link from 'next/link';
+import { sortByDate } from '../../../utils';
+import { Layout } from "../../../components/Layout"
+import Post from '../../../components/Post';
+import { POSTS_PER_PAGE } from '../../../config';
+import Pagination from '../../../components/Pagination';
+
+export default function BlogPage({ posts, numPages, currentPage }) {
+    return (
+        <Layout>
+            <h1 className='text-5xl border-b-4 p-5 font-bold'>Blog</h1>
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+                {posts.map((post, index) => (
+                    <Post key={index} post={post} />
+                )
+                )}
+            </div>
+            <Link className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5
+      transition duration-500 ease select-none hover:text-white
+      hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full' href={'/'}>Back to home page</Link>
+            <Pagination currentPage={currentPage} numPages={numPages}/>
+        </Layout>
+    )
+}
+
+export async function getStaticPaths() {
+    const files = fs.readdirSync(path.join('posts'));
+    //stranice dobijamo tako sto podelimo postove sa njihovim prikazom po strani
+    const numPages = Math.ceil(files.length / POSTS_PER_PAGE);
+
+    let paths = [];
+    for (let i = 1; i <= numPages; i++) {
+        paths.push({
+            //pravimo paginaciju na osnovu broja stranica
+            params: { page_index: i.toString() }
+        })
+    }
+    return {
+        paths,
+        fallback: false
+    }
+
+}
+
+export async function getStaticProps({ params }) {
+    const page = parseInt((params && params.page_index) || 1)
+    //ucitavamo direktorijum i pravimo niz od naziva njegovih fajlova
+    const files = fs.readdirSync(path.join('posts'));
+    //mapiramo niz naziva ovih fajlova
+    const posts = files.map(filename => {
+        //za svaki slug uzimamo naziv mapiranog fajla bez ekstenzije .md
+        const slug = filename.replace('.md', '');
+        //ucitavamo svaki fajl
+        const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
+        //pozivamo matter funkciju iz gray-matter biblioteke
+        const { data: frontmatter } = matter(markdownWithMeta);
+        //fracamo slug i frontmatter sto je ucitani naziv fajla
+        return {
+            slug,
+            frontmatter
+        }
+    })
+    const numPages = Math.ceil(files.length / POSTS_PER_PAGE);
+    const pageIndex = page - 1;
+    const orderedPosts = posts.sort(sortByDate).slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE);
+    //mapirani niz posts gde koristimo module, vracamo kao staticki props
+    //
+    return {
+        props: {
+            posts: orderedPosts,
+            numPages,
+            currentPage: page
+        },
+    }
+}
